9b82275bad8d0ddeac60d4d2ad0ad48f4a5a0cfc
Levitan77 <chipposung@gmail.com>
2024-10-29 11:40:59 +0300

    Динамическая типизация

--- 'module_1_3.py'.py
+++ 'module_1_3.py'.py
@@ -1,10 +1,9 @@
-name = 'Alexey'
-print('Name:' + name)
+name = "Alexey"
+print(name)
 age = 37
-print('Age:', age)
-age = (age+1)
-new_age = 38
-print('New age:', age)
-# is_student= age<age+1
-is_student = new_age == age
-print('Is_student:', is_student)
+print(age)
+new_age = (age + 1)
+print(new_age)
+is_student = True
+print(is_student)
+print("Name:", name, "\nAge:", age, "\nNew age:", new_age, "\nIs Student:", is_student)
