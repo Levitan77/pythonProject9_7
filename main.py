e8c965361496115156609a3c924e1ad31549f49c
Levitan77 <chipposung@gmail.com>
2024-10-29 21:08:02 +0300

    4th program

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-print('Hi, PyCharm')
+#"1st program"
+print(9**0.5*5)
 
-x = 43
+#"2nd program"
+print(9.99>9.98)and(1000!=100.1)
 
-y = 32
+#"3rd program"
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2==2*(2+2))
 
-print(x * y)
-
-print("End line")
+#"4th program"
+print(int((float('123.456'))*10)%10)
