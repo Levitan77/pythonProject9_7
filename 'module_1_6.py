37edcd4939ffa34bca335776619b097a1a748c60
Levitan77 <chipposung@gmail.com>
2024-11-05 15:36:41 +0300

    removed_year.....

--- 'module_1_6.py
+++ 'module_1_6.py
@@ -2,14 +2,17 @@ my_dict={'Alexander': 1990, 'Anna':2002,'Victor':2020}
 print(my_dict)
 print(my_dict['Victor'])
 print(my_dict.get('Anna'),': None')
-my_dict.update({'Anna':2002,
-               'Victor':2020})
-Year_birth=my_dict.pop('Victor')
-print(Year_birth)
+my_dict.update({'Герман':2001,
+               'Анастасия':2015})
+removed_year = my_dict.pop('Анастасия')
+print(removed_year)
 print(my_dict)
-my_set={1,1,2,"Яблоко",23,34,24}
-print(my_set)
-my_set.discard(24)
-my_set.add(72)
-my_set.add(64)
-print(my_set)
+
+print()
+
+my_set = {2, 3, 3, 2, 5, True, True, False, True, 'list', 'set', 'list', 'list'}
+print('Множество:', my_set)
+my_set.add('string')
+my_set.add('float')
+my_set.discard(2)
+print('Изменённое множество:', my_set)
