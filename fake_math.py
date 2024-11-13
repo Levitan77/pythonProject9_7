694e82aba4dc9580d2f113e0c22c2e347d3141f0
Levitan77 <chipposung@gmail.com>
2024-11-13 18:35:57 +0300

    Модули и пакеты

--- fake_math.py
+++ fake_math.py
@@ -1,7 +1,12 @@
-def fake_divide(first, second):
-    if second == 0:
-        return 'Ошибка!!!'
+from module_4_1 import *
+from true_math import *
 
-    else:
-        result = first / second
-        return result
+
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
