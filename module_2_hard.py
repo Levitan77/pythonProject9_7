5d7b889b25a2b9faf07e1afc1d1505a3fcfea898
Levitan77 <chipposung@gmail.com>
2024-11-22 21:06:05 +0300

    Дополнительное практическое задание по модулю: "Основные операторы"

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,21 +1,10 @@
-from random import randint
+n = int(input("Введите число от 3 до 20: "))
 
+result = ""
 
-def trap(n):
-    unique = ''
-    for i in range(1, n):
-        for j in range(i + 1, n + 1):
-            if n % (i + j) == 0:
-                unique += str(i) + str(j)
-    return f'{n} - {unique}\nПоздравляю вы не станете лепешкой'
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if i + j == n:
+            result += str(i) + str(j)
 
-
-#  way 1
-# n = randint(3, 20)
-# print('Случайность решит твою судьбу')
-
-#  way 2
-n = int(input("Привет Эйс Вентура. введи число и если оно будет кратно сумме каждой пары, то вы будете програмистом ;): "))
-
-Ace_Ventura = trap(n)
-print(Ace_Ventura)
+print(result)
