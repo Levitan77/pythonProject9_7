ec5a3819a38d32a38b991d19ea492e4f91dd4064
Levitan77 <chipposung@gmail.com>
2024-12-11 10:30:56 +0300

    "Перегрузка операторов."

--- module_5_3.py
+++ module_5_3.py
@@ -1,113 +1,89 @@
-#  1. Создаем класс House.
+# Домашняя работа по уроку "Перегрузка операторов."
+
+# Задача "Нужно больше этажей":
+
+# Для решения этой задачи будем пользоваться решением к предыдущей задаче "Специальные методы класса".
+#
+# Необходимо дополнить класс House следующими специальными методами:
+# 1 __eq__(self, other) - должен возвращать True, если количество этажей одинаковое у self и у other.
+# 2 Методы __lt__(<), __le__(<=), __gt__(>), __ge__(>=), __ne__(!=) должны присутствовать в классе и возвращать результаты сравнения по соответствующим операторам. Как и в методе __eq__ в сравнении участвует кол-во этажей.
+# 3 __add__(self, value) - увеличивает кол-во этажей на переданное значение value, возвращает сам объект self.
+# 4 __radd__(self, value), __iadd__(self, value) - работают так же как и __add__ (возвращают результат его вызова).
+# Остальные методы арифметических операторов, где self - x, other - y:
+#
+# Следует заметить, что other может быть не только числом, но и вообще любым объектом другого класса.
+# Для более точной логики работы методов __eq__, __add__  и других методов сравнения и арифметики перед выполняемыми
+# действиями лучше убедиться в принадлежности к типу при помощи функции isinstance:
+# isinstance(other, int) - other указывает на объект типа int.
+# isinstance(other, House) - other указывает на объект типа House.
+
+
 class House:
-    #  2. Внутри класса House определяем метод __init__, в который передадаем название и кол-во этажей.
     def __init__(self, name, number_of_floors):
-        #  3. Внутри метода __init__ создаем атрибуты объекта self.name и self.number_of_floors,
-        #  присваиваем им переданные значения.
-        self.name = name
-        self.number_of_floors = number_of_floors
+       self.name = name
+       self.number_of_floors = number_of_floors
 
-    #  4. Создаем метод go_to с параметром new_floor и записываем логику внутри него на основе описания задачи 5_1.
-    def go_to(self, new_floor):
-        #  Если же new_floor больше чем self.number_of_floors или меньше 1,
-        #  то вывести строку "Такого этажа не существует".
-        if 1 < new_floor < self.number_of_floors:
-            for i in range(1, new_floor + 1):
-                print(i)
-            else:
-                print('Такого этажа не существует')
-
-    # 6. Дополняем класс House методом возврата кол-ва этажей здания self.number_of_floors
     def __len__(self):
-        return self.number_of_floors
-
-    # 7. Дополняем класс House методом возврата строки: "Название: <название>, кол-во этажей: <этажи>"
+         return self.number_of_floors
     def __str__(self):
-        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}.'
+       title = str(f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
+       return title
 
-    # Дополняем класс House следующими специальными методами:
-    # 1. Методом сравнения на равенства: возвращаем True, если количество этажей self == other
-    def __eq__(self, other):
-        if isinstance(other, House):
+    def __eq__(self, other):    # 1
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
             return self.number_of_floors == other.number_of_floors
-        else:
-            return False
 
-    # 2.1. Методом сравнения "меньше чем" (Lower than)
-    def __lt__(self, other):
-        if isinstance(other, House):
+    def __lt__(self, other):    # 2
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
             return self.number_of_floors < other.number_of_floors
-        else:
-            return NotImplemented
 
-    # 2.2. Методом сравнения "меньше или равно"
     def __le__(self, other):
-        if isinstance(other, House):
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
             return self.number_of_floors <= other.number_of_floors
-        else:
-            return NotImplemented
 
-    # 2.3. Методом сравнения "больше чем" (Greater than)
     def __gt__(self, other):
-        if isinstance(other, House):
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
             return self.number_of_floors > other.number_of_floors
-        else:
-            return NotImplemented
 
-    # 2.4. Методом сравнения "больше или равно"
     def __ge__(self, other):
-        if isinstance(other, House):
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
             return self.number_of_floors >= other.number_of_floors
-        else:
-            return NotImplemented
 
-    # 2.5. Методом сравнения на неравенство
     def __ne__(self, other):
-        if isinstance(other, House):
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
             return self.number_of_floors != other.number_of_floors
-        else:
-            return True
 
-    # 3. Методом добавления элемента в множество
     def __add__(self, value):
         if isinstance(value, int):
-            return House(self.name, self.number_of_floors + value)
-        elif isinstance(value, House):
-            return House(self.name, self.number_of_floors + value.number_of_floors)
-        else:
-            return NotImplemented
+            self.number_of_floors = self.number_of_floors + value
+        return self
 
-    # 4.1. Методом симметричного сложения
     def __radd__(self, value):
         return self.__add__(value)
 
-    # 4.2. Методом сложения с присваиванием +=
     def __iadd__(self, value):
-        return self.__add__(value)
+        if isinstance(value, int):
+            self.number_of_floors += value
+        return self
 
 
-#  5. Создаем объект класса House с названием и количеством этажей из примера.
-h1 = House('ЖК Эльбрус', 10)
-h2 = House('ЖК Акация', 20)
+hightower = House('Башня', 12)
+warehouse = House('Склад', 4)
 
-# Вывод на консоль:
-print(h1)
-print(h2)
-
-print(h1 == h2)  # __eq__
-
-h1 = h1 + 10  # __add__
-print(h1)
-print(h1 == h2)
-
-h1 += 10  # __iadd__
-print(h1)
-
-h2 = 10 + h2  # __radd__
-print(h2)
-
-print(h1 > h2)  # __gt__
-print(h1 >= h2)  # __ge__
-print(h1 < h2)  # __lt__
-print(h1 <= h2)  # __le__
-print(h1 != h2)  # __ne__
+print(hightower)
+print(warehouse)
+# 1
+print(hightower == warehouse)
+# 3
+print(warehouse.__add__(8))
+print(hightower == warehouse)
+# 2
+print(hightower < warehouse)
+print(hightower <= warehouse)
+print(hightower > warehouse)
+print(hightower >= warehouse)
+print(hightower != warehouse)
+# 5
+print(hightower.__radd__(7))
+print(warehouse.__iadd__('8')) # не прибавляется
+print(warehouse.__iadd__(8))
