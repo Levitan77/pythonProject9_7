9e40db062f2bf01ac98f7ab10e2537fc2447633d
Levitan77 <chipposung@gmail.com>
2024-12-11 10:25:27 +0300

    "Специальные методы классов"

--- module_5_2.py
+++ module_5_2.py
@@ -1,39 +1,31 @@
-#  1. Создаем класс House.
+# Домашняя работа по уроку "Специальные методы классов"
+
+# Задача "Магические здания":
+# Для решения этой задачи будем пользоваться решением к предыдущей задаче "Атрибуты и методы объекта".
+#
+# Необходимо дополнить класс House следующими специальными методами:
+# 1 __len__(self) - должен возвращать кол-во этажей здания self.number_of_floors.
+# 2 __str__(self) - должен возвращать строку: "Название: <название>, кол-во этажей: <этажи>".
+
+
 class House:
-    #  2. Вунтри класса House определяем метод __init__, в который передадаем название и кол-во этажей.
     def __init__(self, name, number_of_floors):
-        #  3. Внутри метода __init__ создаем атрибуты объекта self.name и self.number_of_floors,
-        #  присваиваем им переданные значения.
         self.name = name
         self.number_of_floors = number_of_floors
 
-    #  4. Создаем метод go_to с параметром new_floor и записываем логику внутри него на основе описания задачи 5_1.
-    def go_to(self, new_floor):
-        if 1 < new_floor < self.number_of_floors:
-            for i in range(1, new_floor + 1):
-                print(i)
-            else:
-                print('Такого этажа не существует')
-
-    # 6. Дополняем класс House методом возврата кол-ва этажей здания self.number_of_floors
     def __len__(self):
         return self.number_of_floors
 
-    # 7. Дополняем класс House методом возврата строки: # "Название: <название>, кол-во этажей: <этажи>"
     def __str__(self):
-        return f"Название: {self.name}, количество этажей: {self.number_of_floors}"
+        title = str(f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
+        return title
 
 
-#  5. Создаем объект класса House с названием и количеством этажей из примера.
-h1 = House('ЖК Эльбрус', 10)
-h2 = House('ЖК Акация', 20)
+hightower = House('Башня', 12)
+warehouse = House('Склад', 4)
 
-#  Выводим на консоль строки
-# __str__
-print(h1)
-print(h2)
+print(hightower)
+print(warehouse)
 
-#  Выводим на консоль кол-ва этажей зданий
-# __len__
-print(len(h1))
-print(len(h2))
+print(len(hightower))
+print(len(warehouse))
